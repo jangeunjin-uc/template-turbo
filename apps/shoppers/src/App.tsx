77978f02cfbee868673c27ddfc67c926c537b563
FADE <newvaaaa@gmail.com>
2023-08-27 21:25:59 +0900

    chore : apps/shoppers 추가

---
+++ apps/shoppers/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "ui";
+
+function App() {
+  return <div>
+    <Button variant="textOnly" label="button" />
+  </div>;
+}
+
+export default App;
---
+++ packages/ui/src/components/Button/index.tsx
@@ -0,0 +1,49 @@
+import classNames from "classnames";
+import { ButtonHTMLAttributes, ForwardedRef, ReactNode, forwardRef } from "react";
+
+export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "css"> {
+  variant: "textOnly" | "iconOnly" | "textIcon";
+  styleType?: "contained" | "outline" | "ghost";
+  label?: string;
+  icon?: ReactNode;
+  textIconPosition?: "left" | "right";
+  buttonSize?: "xlarge" | "large" | "medium" | "small";
+  buttonColor?: "primary" | "secondary" | "blue";
+}
+
+function Button(
+  {
+    className,
+    children,
+    variant,
+    label = undefined,
+    icon = undefined,
+    styleType = undefined,
+    textIconPosition = undefined,
+    buttonSize = undefined,
+    buttonColor = undefined,
+    ...rest
+  }: ButtonProps,
+  ref: ForwardedRef<HTMLButtonElement>
+) {
+  return (
+    <button
+      type="button"
+      className={classNames("btn", [className])}
+      {...rest}
+      ref={ref}
+      data-variant={variant}
+      data-style-type={styleType}
+      data-icon-position={textIconPosition}
+      data-size={buttonSize}
+      data-color={buttonColor}
+    >
+      {variant === "textIcon" && textIconPosition === "left" && icon}
+      {variant !== "iconOnly" ? <span className="btn__label">{label}</span> : icon}
+      {variant === "textIcon" && textIconPosition === "right" && icon}
+      {children}
+    </button>
+  );
+}
+
+export default forwardRef(Button);
