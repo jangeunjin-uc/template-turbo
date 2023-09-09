53aaf25993b06690cf8d67c2267bf0d3ce844849
FADE <newvaaaa@gmail.com>
2023-09-09 14:50:01 +0900

    feature : apps/shoppers store toast 추가

--- apps/shoppers/src/store/index.ts
+++ apps/shoppers/src/store/index.ts
@@ -2,9 +2,11 @@ import { StateCreator, create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import { ModalSliceType, modalSlice } from './modal';
+import { ToastSliceType, toastSlice } from './toast';
 
 interface StoreType {
   modal: ModalSliceType;
+  toast: ToastSliceType;
 }
 
 type MiddleWareType = [['zustand/devtools', never], ['zustand/immer', never]];
@@ -15,6 +17,7 @@ export const store = create<StoreType>()(
   devtools(
     immer((...args) => ({
       modal: modalSlice(...args),
+      toast: toastSlice(...args),
     })),
   ),
 );
